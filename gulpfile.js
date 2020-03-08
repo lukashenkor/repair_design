bc486e8acedc3e2450a3a52de1c884054eb4a9e6
lukashenko_r <ramosss1997@gmail.com>
2020-03-08 11:29:18 +0300

    feat: add Futura Book & Demi fonts

--- gulpfile.js
+++ gulpfile.js
@@ -15,17 +15,17 @@ function bs() {
     watch("./*.html").on('change', browserSync.reload);
     watch("./sass/**/*.sass", serveSass);
     watch(".js/*.js").on('change', browserSync.reload);
-};
+}
 
 // Compile sass into CSS & auto-inject into browsers
 function serveSass() {
-    return src("./sass/*.sass")
+    return src("./sass/**/*.sass")
         .pipe(sass())
         .pipe(autoprefixer({
             cascade: false
         }))
         .pipe(dest("./css"))
         .pipe(browserSync.stream());
-};
+}
 
 exports.serve = bs;
